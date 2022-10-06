dbdaa2c8ef2ff7f281eb4f5940c18867fef87ed2
nadiaputrirahmawati <114629996+nadiaputrirahmawati@users.noreply.github.com>
2022-10-06 09:14:39 +0700

    Add files via upload

---
+++ tugaspemogramantiga.py
@@ -0,0 +1,7 @@
+#tugas sesi 3 by nadia putri rahmawati 
+nama = "nadia putri rahmawati"
+kelas = "TI22A"
+jurusan = "teknik informatika"
+quote = "jangan ada kata nanti di antara kita ya, kerjakan"
+
+print(nama, kelas, jurusan, quote)
