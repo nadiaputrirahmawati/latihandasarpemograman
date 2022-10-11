2ee9e826f178330e21a737c6ed6e3537ae7280a0
nadiaputrirahmawati <114629996+nadiaputrirahmawati@users.noreply.github.com>
2022-10-11 16:43:35 +0700

    Add files via upload

---
+++ Tugassesi3nadiaputri.py
@@ -0,0 +1,12 @@
+#tugas sesi 3 by nadia putri rahmawati 
+nama = "nadia putri rahmawati"
+kelas = "TI22A"
+jurusan = "teknik informatika"
+quote = "jangan ada kata nanti di antara kita ya, kerjakan :)"
+note = "Udah Bisa Yaa"
+
+print(nama)
+print(kelas)
+print(jurusan)
+print(quote)
+print(note)
